db5a8c50d36e64bd300bda294061e480d14943be
manas <manaspradhan567@gmail.com>
2025-09-02 01:12:20 +0530

    Refactor EKS worker node configuration and add Jenkins deployment resources

--- worker-node.tf
+++ worker-node.tf
@@ -1,34 +1,23 @@
-# Launch Template for Worker Nodes
-resource "aws_launch_template" "eks_workers" {
-  name_prefix   = "eks-workers"
-  image_id      = "ami-07802869d4ef48dbe"
-  instance_type = "t3.medium"
+resource "aws_eks_node_group" "managed_workers" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "managed-workers"
+  node_role_arn   = aws_iam_role.eks_worker_role.arn
 
-  iam_instance_profile {
-    name = aws_iam_instance_profile.eks_worker_profile.name
-  }
-}
+  subnet_ids = [
+    aws_subnet.eks_private_1a.id,
+    aws_subnet.eks_private_1b.id
+  ]
 
-resource "aws_iam_instance_profile" "eks_worker_profile" {
-  name = "eks-worker-profile"
-  role = aws_iam_role.eks_worker_role.name
-}
-
-# Self-managed Auto Scaling Group for Workers
-resource "aws_autoscaling_group" "eks_asg" {
-  desired_capacity     = 2
-  max_size             = 3
-  min_size             = 1
-  vpc_zone_identifier  = [aws_subnet.eks_private_1a.id, aws_subnet.eks_private_1b.id]
-
-  launch_template {
-    id      = aws_launch_template.eks_workers.id
-    version = "$Latest"
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
 
-  tag {
-    key                 = "kubernetes.io/cluster/${aws_eks_cluster.eks.name}"
-    value               = "owned"
-    propagate_at_launch = true
+  instance_types = ["t3.medium"]
+  ami_type       = "BOTTLEROCKET_x86_64"
+
+  tags = {
+    "kubernetes.io/cluster/${aws_eks_cluster.eks.name}" = "owned"
   }
 }
