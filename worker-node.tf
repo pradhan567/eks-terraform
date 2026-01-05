661db0d4ef8d84dc5617853ed02dd07dc3cf1a15
manas <manaspradhan567@gmail.com>
2026-01-05 16:57:45 +0530

    Update EKS configuration: upgrade Kubernetes provider version, remove obsolete EFS resources, and add new managed worker node group

--- worker-node.tf
+++ worker-node.tf
@@ -21,3 +21,27 @@ resource "aws_eks_node_group" "managed_workers" {
     "kubernetes.io/cluster/${aws_eks_cluster.eks.name}" = "owned"
   }
 }
+
+resource "aws_eks_node_group" "managed_workers-1" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "managed-workers"
+  node_role_arn   = aws_iam_role.eks_worker_role.arn
+
+  subnet_ids = [
+    aws_subnet.eks_private_1a.id,
+    aws_subnet.eks_private_1b.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.small"]
+  ami_type       = "BOTTLEROCKET_x86_64"
+
+  tags = {
+    "kubernetes.io/cluster/${aws_eks_cluster.eks.name}" = "owned"
+  }
+}
